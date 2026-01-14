1ea928de4b80a5092d93dc8ce4c07a3ed34d8f11
Funda variş <funda.varis@gmail.com>
2026-01-14 15:41:55 +0100

    typo errors fixed for e2e.yml

--- e2e_tests/auth/test_example.py
+++ e2e_tests/auth/test_example.py
@@ -5,4 +5,4 @@ from e2e_tests.pages.search_page import SearchPage
 def test_go_to_example_page(page):
     page.goto("/")  # uses base_url from context
     start_page = SearchPage(page)
-    expect(start_page.get_started_link).to_have_text("Get startedgit add")
+    expect(start_page.get_started_link).to_have_text("Get started")
