ce92ea02282c0d0475c2886546d8d9edc2056d7e
Funda variş <funda.varis@gmail.com>
2026-01-14 15:18:37 +0100

    playwright traces fixture is added

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -1,13 +1,16 @@
+import pathlib
+
 import pytest
-from playwright.sync_api import sync_playwright, Page
-import utils
+from playwright.sync_api import sync_playwright
+from e2e_tests.utils import settings
+import uuid
 
 CONFIG_PATH = "config.ini"
-
+TRACES_DIR_PATH = pathlib.Path.cwd().joinpath("artifacts").joinpath("traces")
 
 @pytest.fixture(scope="session")
 def config():
-    return utils.load_config(path=CONFIG_PATH)
+    return settings.load_config(path=CONFIG_PATH)
 
 
 @pytest.fixture(scope="function")
@@ -15,12 +18,14 @@ def browser(config):
     ui_config = config["ui"]
     browser_name = ui_config.get("browser_name")
     with sync_playwright() as p:
-        if browser_name == "chrome":
-            browser = p.chromium.launch(headless=ui_config.get("headless"))
+        if browser_name == "chromium":
+            browser = p.chromium.launch(headless=ui_config.getboolean("headless"))
         elif browser_name == "firefox":
-            browser = p.firefox.launch(headless=ui_config.get("headless"))
+            browser = p.firefox.launch(headless=ui_config.getboolean("headless"))
         elif browser_name == "webkit":
-            browser = p.webkit.launch(headless=ui_config.get("headless"))
+            browser = p.webkit.launch(headless=ui_config.getboolean("headless"))
+        else:
+            raise RuntimeError(f"Unknown browser: {browser_name}")
 
         yield browser
         browser.close()
@@ -31,5 +36,28 @@ def browser_context(config, browser):
     ui_config = config["ui"]
     base_url = ui_config.get("base_url")
     ctx = browser.new_context(base_url=base_url)
+
     yield ctx
     ctx.close()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def trace_browser_context(config, browser_context):
+    ui_config = config["ui"]
+    save_trace = ui_config.getboolean("save_trace")
+
+    if save_trace:
+        browser_context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield
+
+    if save_trace:
+        browser_context.tracing.stop(path=TRACES_DIR_PATH.joinpath(f"trace-{uuid.uuid4()}.zip"))
+
+
+@pytest.fixture(scope="function")
+def page(browser_context):
+    page = browser_context.new_page()
+    yield page
+
+    page.close()
