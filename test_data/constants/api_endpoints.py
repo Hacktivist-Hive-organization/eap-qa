424017df1176c5fd684d892c74d888a60ce39b41
Funda variş <funda.varis@gmail.com>
2026-02-11 13:57:44 +0100

    adding request endpoints

--- test_data/constants/api_endpoints.py
+++ test_data/constants/api_endpoints.py
@@ -11,3 +11,11 @@ class UserEndpoints:
     USERS = "/api/v1/users/"
     USERS_ME = "/api/v1/users/me"
     USER_BY_ID = "/api/v1/users/{id}"
+
+
+class RequestsEndpoints:
+    REQUESTS = "/api/v1/requests/"
+    MY_REQUESTS = "/api/v1/requests/my-requests"
+    GET_REQUESTS = "/api/v1/requests/{request_id}"
+    REQUEST_TYPES = "/api/v1/types/"
+    REQUEST_SUBTYPES = "/api/v1/subtypes/"
