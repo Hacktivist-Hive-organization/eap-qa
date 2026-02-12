57df752ebf73534454bee1b3cfa831e144ab7b44
Funda variş <funda.varis@gmail.com>
2026-02-12 17:45:51 +0100

    update logout action

--- e2e_tests/ui/pages/requests.py
+++ e2e_tests/ui/pages/requests.py
@@ -1,11 +1,14 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
 class RequestsPage:
     def __init__(self, page: Page):
         self.page = page
         self.page_title = page.get_by_text('all Requests Dashboard')
-        self.logout_button = page.get_by_role('button', name='Logout')
+        self.account_button = page.locator('[data-slot="dropdown-menu-trigger"]')
+        self.logout_button = page.get_by_role("menuitem", name='Logout')
 
     def click_logout_button(self):
+        self.account_button.click()
+        expect(self.logout_button).to_be_visible()
         self.logout_button.click()
