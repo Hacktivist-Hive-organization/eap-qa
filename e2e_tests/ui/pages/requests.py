65198925ad1aaa3af3adf19c57a2f86740b6624e
Anastasiia Motopp <anastasiia.ryzhkova@motopp.nl>
2026-03-18 09:33:08 +0100

    case fix

--- e2e_tests/ui/pages/requests.py
+++ e2e_tests/ui/pages/requests.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Page, expect
 class RequestsPage:
     def __init__(self, page: Page):
         self.page = page
-        self.page_title = page.get_by_text('all Requests Dashboard')
+        self.page_title = page.get_by_text('All Requests Dashboard')
         self.account_button = page.locator('[data-slot="dropdown-menu-trigger"]')
         self.logout_button = page.get_by_role("menuitem", name='Logout')
 
